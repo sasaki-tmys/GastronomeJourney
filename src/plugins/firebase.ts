1148b650fe03f118ed40877981e17d31518f786d
sasaki-tmys <sasaki_tmys@es-planning.jp>
2024-04-05 16:58:29 +0900

    環境変数へ置き換え

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDjeg2plvxULXo1ae2X7z07yRy38n20g8',
-  authDomain: 'gjourney-project.firebaseapp.com',
-  projectId: 'gjourney-project',
-  storageBucket: 'gjourney-project.appspot.com',
-  messagingSenderId: '596838736327',
-  appId: '1:596838736327:web:b9acb31727de9a08ae79e2',
-  measurementId: 'G-DCB40LXS7E'
+  apiKey: import.meta.env,
+  authDomain: import.meta.env,
+  projectId: import.meta.env,
+  storageBucket: import.meta.env,
+  messagingSenderId: import.meta.env,
+  appId: import.meta.env,
+  measurementId: import.meta.env
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
